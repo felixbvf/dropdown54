7444464c3dda23aee6b218406610edb577c4585d
felixbvf <felixbvf@gmail.com>
2017-01-30 21:05:09 -0400

    Added LaravelCollective, create View and Upgraded HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class HomeController extends Controller
 {
@@ -21,8 +22,18 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+     public function getViewModel() {
+         $users = User::All();
+         $users_list = array('' =>'');
+         foreach ($users as $item) {
+             $users_list[$item->id]=$item->name;
+         }
+         return [
+             'users_list' => $users_list
+         ];
+     }
     public function index()
     {
-        return view('home');
+        return view('home', self::getViewModel());
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,10 +8,43 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    You are logged
+
                 </div>
             </div>
         </div>
     </div>
+    {!! Form::open(['method' => 'GET', 'route' => ['goy.index'], 'class' => 'form-horizontal']) !!}
+    <div class="row">
+        <div class="col-md-6">
+            <div class="box box-warning box-solid">
+                <div class="box-header with-border">
+                    <h3 class="panel-title">Datos Personales</h3>
+                    <div class="box-body">
+						<div class="row">
+							<div class="col-md-12">
+								<div class="form-group">
+									{!! Form::label('module', 'Unidad', ['class' => 'col-md-4 control-label']) !!}
+									<div class="col-md-6">
+										{!! Form::select('module', $users_list, null, ['class' => 'form-control', 'required' => 'required']) !!}
+										<span class="help-block">Selecione la Unidad correspondiente</span>
+									</div>
+								</div>
+								<div class="form-group">
+									{!! Form::label('role', 'Cargo', ['class' => 'col-md-4 control-label']) !!}
+									<div class="col-md-6">
+										{!! Form::select('role', ['clear' => ''], null, ['class' => 'form-control', 'required' => 'required']) !!}
+
+										<span class="help-block">Selecione el Cargo</span>
+									</div>
+								</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    {!! Form::close() !!}
 </div>
 @endsection
