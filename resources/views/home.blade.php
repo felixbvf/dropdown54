1c0a034bce2c8e4065eed767dc2601f109c44649
felixbvf <felixbvf@gmail.com>
2017-02-17 12:06:35 -0400

    bug fixes components and slot

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.app')
+@component('layouts.app')
 
-@section('content')
+@slot('content')
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -22,8 +22,8 @@
             </div>
         </div>
     </div>
-
-@endsection
+@endslot
+@endcomponent
 <script>
 
 
